ce6f045787ea2dac5b99bcdafa5e1fe4e350f1f9
Adam Walsh <adamwalsh14@gmail.com>
2015-10-25 17:11:15 -0700

    use zshrc (install oh-my-zsh), add thefuck and pathogen installs

--- setup.sh
+++ setup.sh
@@ -5,9 +5,13 @@ cd "$(dirname "${BASH_SOURCE}")";
 git pull origin master;
 
 function doIt() {
+  sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" # install oh-my-zsh
+  wget -O - https://raw.githubusercontent.com/nvbn/thefuck/master/install.sh | sh - && $0 # install thefuck
+  mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+  curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim # install pathogen
   rsync --exclude ".git/" --exclude ".DS_Store" \
     --exclude "README.md" --exclude "LICENSE.txt" -avh --no-perms . ~;
-  source ~/.bash_profile;
+  source ~/.zshrc;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
