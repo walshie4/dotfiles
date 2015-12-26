cfbf24b9b2197d6b84014b325c95e641778447e2
Adam Walsh <adamwalsh14@gmail.com>
2015-12-26 01:32:25 -0500

    Use ssh clone paths

--- setup.sh
+++ setup.sh
@@ -11,23 +11,22 @@ function doIt() {
   sudo pip install psutil thefuck # install thefuck on Ubuntu
   brew install thefuck # install thefuck on OS X
   mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-  ./update_pathogen.sh # install / update pathogen
-  git clone https://github.com/junegunn/vim-easy-align.git  ~/.vim/bundle/vim-easy-align # install pathogen plugins
-  git clone https://github.com/ap/vim-css-color.git         ~/.vim/bundle/vim-css-color
-  git clone https://github.com/kchmck/vim-coffee-script.git ~/.vim/bundle/vim-coffee-script
-  git clone https://github.com/wavded/vim-stylus.git        ~/.vim/bundle/vim-stylus
-  git clone https://github.com/Bling/vim-airline.git        ~/.vim/bundle/vim-airline
-  git clone https://github.com/tpope/vim-surround.git       ~/.vim/bundle/vim-surround
-  git clone https://github.com/mustache/mustache.git        ~/.vim/bundle/mustache
-  git clone https://github.com/vbundles/nerdtree.git        ~/.vim/bundle/nerdtree
-  git clone https://github.com/mattn/emmet-vim.git          ~/.vim/bundle/emmet-vim
-  git clone https://github.com/scrooloose/syntastic.git     ~/.vim/bundle/syntastic
-  git clone https://github.com/ervandew/supertab.git        ~/.vim/bundle/supertab
-  git clone https://github.com/tpope/Vim-fugitive.git       ~/.vim/bundle/Vim-fugitive
-  git clone https://github.com/godlygeek/tabular.git        ~/.vim/bundle/tabular
-  git clone https://github.com/vim-scripts/matchit.zip.git  ~/.vim/bundle/matchit
-  git clone https://github.com/Yggdroot/indentLine.git      ~/.vim/bundle/indentLine
-  git clone https://github.com/wincent/command-t.git        ~/.vim/bundle/command-t
+  git clone git@github.com:junegunn/vim-easy-align.git  ~/.vim/bundle/vim-easy-align # install pathogen plugins
+  git clone git@github.com:ap/vim-css-color.git         ~/.vim/bundle/vim-css-color
+  git clone git@github.com:kchmck/vim-coffee-script.git ~/.vim/bundle/vim-coffee-script
+  git clone git@github.com:wavded/vim-stylus.git        ~/.vim/bundle/vim-stylus
+  git clone git@github.com:Bling/vim-airline.git        ~/.vim/bundle/vim-airline
+  git clone git@github.com:tpope/vim-surround.git       ~/.vim/bundle/vim-surround
+  git clone git@github.com:mustache/mustache.git        ~/.vim/bundle/mustache
+  git clone git@github.com:vbundles/nerdtree.git        ~/.vim/bundle/nerdtree
+  git clone git@github.com:mattn/emmet-vim.git          ~/.vim/bundle/emmet-vim
+  git clone git@github.com:scrooloose/syntastic.git     ~/.vim/bundle/syntastic
+  git clone git@github.com:ervandew/supertab.git        ~/.vim/bundle/supertab
+  git clone git@github.com:tpope/Vim-fugitive.git       ~/.vim/bundle/Vim-fugitive
+  git clone git@github.com:godlygeek/tabular.git        ~/.vim/bundle/tabular
+  git clone git@github.com:vim-scripts/matchit.zip.git  ~/.vim/bundle/matchit
+  git clone git@github.com:Yggdroot/indentLine.git      ~/.vim/bundle/indentLine
+  git clone git@github.com:wincent/command-t.git        ~/.vim/bundle/command-t
   mkdir -p ~/.vim/autoload ~/.vim/bundle && \
   ./update_pathogen.sh # install / update pathogen again to update plugins
   sudo chsh -s $(which zsh) $(whoami) # change shell to zsh
